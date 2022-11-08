1216911371b7333e8e0686c643ac1dac5023f05e
multi-clai <69476405+multi-clai@users.noreply.github.com>
2022-11-08 17:27:37 -0500

    Test first animation

--- test.js
+++ test.js
@@ -1,6 +1,12 @@
 gsap.registerPlugin(ScrollTrigger);
 
 gsap.to("#first-item",{
+    scrollTrigger: {
+        trigger:".first-slide",
+        pin: true,
+        start: "top top",
+        scrub: true,
+    },
     duration:2,
     opacity: 0
 });
