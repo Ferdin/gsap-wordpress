3c759635552bd71a5e071f98305c166c39c063b9
multi-clai <69476405+multi-clai@users.noreply.github.com>
2022-11-10 16:07:04 -0500

    Add scrub pin and fade

--- test.js
+++ test.js
@@ -1,12 +1,26 @@
 gsap.registerPlugin(ScrollTrigger);
 
-gsap.to("#first-item",{
-    scrollTrigger: {
-        trigger:".first-slide",
-        pin: true,
-        start: "top top",
-        scrub: true,
-    },
+let tl = gsap.timeline();
+
+
+tl.to("#first-item",{
     duration:2,
     opacity: 0
 });
+
+tl.to("#first-item-description",{
+    x: -40,
+    duration:2,
+    opacity:0
+});
+
+ScrollTrigger.create({
+    animation: tl,
+    trigger:".first-slide",
+    pin: true,
+    start: "top top",
+    snap: 1,
+    // end: "bottom top",
+    scrub: 1,
+});
+
