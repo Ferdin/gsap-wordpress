c114f2d1f917bec587c7cc95db63cb783419dd2b
multimatic-ferdin <fnorbert@multimatic.com>
2023-06-16 17:43:09 -0400

    images and fonts

--- drinks.js
+++ drinks.js
@@ -1,18 +1,33 @@
 const cursor = document.querySelector('.circle');
-    
+let image = document.querySelector('.headset')    
 document.addEventListener('mousemove', (e) => {
     gsap.to(cursor, {
-    duration: 0.3,
+    duration: 1.5,
     x: e.clientX,
-    y: e.clientY
+    y: e.clientY,
+    ease: "elastic.out"
     });
 });
 
 gsap.to(cursor, {
     duration: 2,
-    scale: 1.5,
+    scale: 1.1,
     opacity: 1,
     repeat: -1,
     yoyo: true,
     ease: "power1.inOut"
+});
+
+image.addEventListener('mouseover', () => {
+    gsap.to(image,{
+        rotation: 4
+    }
+    )
+})
+
+image.addEventListener('mouseleave', () => {
+    gsap.to(image, {
+      rotation: 0
+    });
   });
+
