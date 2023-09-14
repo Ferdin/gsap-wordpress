cd475f2b98f5ff471d5ab03a7726563be90de703
multimatic-ferdin <fnorbert@multimatic.com>
2023-09-14 15:49:48 -0400

    Add more animmation

--- adbanner.js
+++ adbanner.js
@@ -1,16 +1,25 @@
 let strip = document.querySelector(".strip");
 let blackBG = document.querySelector(".black-bg");
 let image = document.querySelector(".image");
+let texts = gsap.utils.toArray('.discount')
 
-function removeElement(element) {
-    if (typeof(element) === "string") {
-      element = document.querySelector(element);
-    }
-    return function() {
-      element.parentNode.removeChild(element);
-    };
-}
+// function removeElement(element) {
+//     if (typeof(element) === "string") {
+//       element = document.querySelector(element);
+//     }
+//     return function() {
+//       element.parentNode.removeChild(element);
+//     };
+// }
 
+gsap.set(image, {
+    display: 'none',
+    scaleY: 0
+})
+
+gsap.set(texts, {
+    opacity: 0
+})
 
 let tl = gsap.timeline();
 
@@ -34,4 +43,26 @@ tl.to(blackBG, {
     duration: 2
 }, "-=2");
 
-let tl2 = gsap.timeline();
+tl.to(texts, {
+    opacity: 1,    
+});
+
+tl.to(texts, {
+    duration: 2
+})
+
+tl.to(texts, {
+    opacity: 0
+})
+
+tl.to(image, {
+    display: 'block',
+    scaleY: 1
+})
+
+
+// let tl2 = gsap.timeline();
+
+// tl2.to(image, {
+//     display: 'block'
+// })
