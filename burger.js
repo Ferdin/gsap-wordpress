dd504b9e1b90bd438157413b5f6cf6b3bc652387
multimatic-ferdin <fnorbert@multimatic.com>
2024-06-03 16:03:31 -0400

    Animation fix

--- burger.js
+++ burger.js
@@ -1,36 +1,45 @@
 var imgElements = document.querySelectorAll('.img-brg');
 
-gsap.from(imgElements.item(0), {
-    duration: 1,
-    y: -200, // Delay each image slightly
-});
+var tl = gsap.timeline();
 
-gsap.from(imgElements.item(1), {
-    duration: 1,
-    y: -150, // Delay each image slightly
-});
+tl.from(imgElements.item(6), {
+    y: -100,
+    ease: "expo.out",
+    duration: 1.
+}, "");
 
-gsap.from(imgElements.item(2), {
-    duration: 1,
-    y: -100, // Delay each image slightly
-});
+tl.from(imgElements.item(5), {
+    y: -1000,
+    ease: "expo.out",
+    duration: 1.5
+}, "-=1.4");
 
-gsap.from(imgElements.item(3), {
-    duration: 1,
-    y: -50, // Delay each image slightly
-});
+tl.from(imgElements.item(4), {
+    y: -1000,
+    ease: "expo.out",
+    duration: 1.5
+}, "-=1.4");
 
-gsap.from(imgElements.item(4), {
-    duration: 1,
-    y: 50, // Delay each image slightly
-});
+tl.from(imgElements.item(3), {
+    y: -1000,
+    ease: "expo.out",
+    duration: 1.5
+}, "-=1.4");
 
-gsap.from(imgElements.item(5), {
-    duration: 1,
-    y: 100, // Delay each image slightly
-});
+tl.from(imgElements.item(2), {
+    y: -1000,
+    ease: "expo.out",
+    duration: 1.5
+}, "-=1.4");
 
-gsap.from(imgElements.item(6), {
-    duration: 1,
-    y: 150, // Delay each image slightly
-});
+tl.from(imgElements.item(1), {
+    y: -1000,
+    ease: "expo.out",
+    duration: 1.5
+}, "-=1.4");
+
+tl.from(imgElements.item(0), {
+    y: -1000,
+    ease: "expo.out",
+    duration: 1.5
+}, "-=1.4");
